488ce4f2b0db6e41e885d213487b0e3e78e02f23
Admin <Admin@DESKTOP-BHPKJLM>
2022-08-19 11:11:07 +0530

    Add project files.

---
+++ linqeg/Program.cs
@@ -0,0 +1,62 @@
+﻿
+using System;
+using System.Collections;
+
+namespace linqeg
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //List<int> number = new List<int>();   
+            //number.Add(32);
+            //number.Add(34);
+            //number.Add(8);
+            //number.Add(4);
+            //number.Add(3);
+            //number.Add(24);
+
+            //// Querry syntax
+            //var result = (from i in number where i>20 select i);
+            //foreach (int i in result)
+            //    Console.WriteLine(i);
+
+            //// Method syntax
+            //Console.WriteLine("Method syntax");
+            //var res = number.Where(i => i > 25).Select(i=>i);
+            //foreach (int i in res)
+            //    Console.WriteLine(i);
+
+            List<EmpDetails> employeedetails = EmpDetails.getEmpDetails();
+
+            foreach(var emp in employeedetails)
+            {
+                Console.WriteLine(emp.ToString());
+            }
+
+            var employee =  employeedetails.Where(x => x.salary > 50000).Select(x => x).ToList();
+            Console.WriteLine("Salary greater than 50k");
+            foreach (var item in employee)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+
+            Console.WriteLine("Enter id: ");
+            int id = int.Parse(Console.ReadLine());
+
+            EmpDetails obedetail =  EmpDetails.getempdetailbyID(id);
+            Console.WriteLine(obedetail.ToString());
+            
+
+
+
+
+
+
+
+
+        }
+    }
+}
+
---
+++ linqeg/empdetails.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linqeg
+{
+    internal class EmpDetails
+    {
+        public int empid { get; set; }
+
+        public string name { get; set; }
+
+        public int empage { get; set; }
+        public float salary { get; set; }
+        public static List<EmpDetails> listemp = new List<EmpDetails>();
+
+
+        public EmpDetails() {
+        }
+
+        public EmpDetails(int empid, string name, int empage, float salary)
+        {
+            this.empid = empid;
+            this.name = name;
+            this.empage = empage;
+            this.salary = salary;
+        }
+        public static List<EmpDetails> getEmpDetails()
+        {
+            listemp.Add(new EmpDetails(12, "rana", 30, 60000));
+            listemp.Add(new EmpDetails(13, "rana", 23, 50000));
+            listemp.Add(new EmpDetails(14, "rana", 21, 50000));
+            listemp.Add(new EmpDetails(15, "rana", 32, 70000));
+
+            return listemp;
+
+        }
+
+        public static EmpDetails getempdetailbyID(int id)
+        {
+            var employee = (from emp in listemp where emp.empid == id select emp).SingleOrDefault();
+            return employee;
+        }
+
+        public override string ToString()
+        {
+            return empid + " " + name + " " + empage + " " + salary;
+        }
+
+
+    }
+}
